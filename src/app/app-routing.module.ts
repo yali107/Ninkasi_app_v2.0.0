4f61b6944b89b807fd90ac45d1b4b4f1769bdeff
yali107 <yuan.li107@gmail.com>
2020-03-11 23:42:53 -0400

    refactor directory, added home page and carousel component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { IntroComponent } from './intro/intro.component';
-import { WorkflowComponent } from './workflow/workflow.component';
-import { TeamComponent } from './team/team.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IntroComponent } from './pages/intro/intro.component';
+import { WorkflowComponent } from './pages/workflow/workflow.component';
+import { TeamComponent } from './pages/team/team.component';
 import { ContentBasedRecComponent } from './ninkasi/content-based-rec/content-based-rec.component';
 import { CollabFilteringRecComponent } from './ninkasi/collab-filtering-rec/collab-filtering-rec.component';
 import { NinkasiComponent } from './ninkasi/ninkasi.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/#', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'intro', component: IntroComponent },
   { path: 'workflow', component: WorkflowComponent },
   { path: 'team', component: TeamComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,24 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { IntroComponent } from './intro/intro.component';
-import { WorkflowComponent } from './workflow/workflow.component';
-import { TeamComponent } from './team/team.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { IntroComponent } from './pages/intro/intro.component';
+import { WorkflowComponent } from './pages/workflow/workflow.component';
+import { TeamComponent } from './pages/team/team.component';
 import { NinkasiModule } from './ninkasi/ninkasi.module';
+import { CarouselComponent } from './shared/components/carousel/carousel.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
     HeaderComponent,
     IntroComponent,
     WorkflowComponent,
     TeamComponent,
+    CarouselComponent,
+    HomeComponent,
     // NinkasiComponent,
     // ContentBasedRecComponent,
     // CollabFilteringRecComponent
